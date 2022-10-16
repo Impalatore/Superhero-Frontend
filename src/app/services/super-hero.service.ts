7452192262c84934ed09b952bd79639bd2afa374
Impalatore <naim7710@gmail.com>
2022-10-16 09:22:36 -0500

    Funcionalidades Terminadas

--- src/app/services/super-hero.service.ts
+++ src/app/services/super-hero.service.ts
@@ -25,6 +25,7 @@ export class SuperHeroService {
   }
 
   public deleteHero(hero : SuperHero) : Observable<SuperHero[]> {
-    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${hero.id}`);
+    return this.http.delete<SuperHero[]>(`${environment.apiUrl}/${this.url}/${hero.id}`);
   }
 }
+
